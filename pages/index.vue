90dede5d0ab6e966ed9452524a07e8fb071f98fd
Roman Silich <46977768+tramadon@users.noreply.github.com>
2025-09-18 16:07:05 -0400

    Add modern Tetris game page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <Tutorial />
+  <TetrisGame />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import TetrisGame from '~/components/TetrisGame.vue'
 
 export default Vue.extend({
   name: 'IndexPage',
+  components: { TetrisGame },
 })
 </script>
